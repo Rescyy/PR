efde08457d8ac0eeabc220f9b652e43e8849ad40
Crucerescu Vladislav <vladislav.crucerescu@quasar-development.com>
2023-10-12 10:16:27 +0200

    lab5

--- lab5/client.py
+++ lab5/client.py
@@ -1,79 +1,118 @@
 import socket
 import json
 import threading
+import os
+import re
 
-# Server configuration
-HOST = '127.0.0.1' # Loopback address for localhost
-PORT = 12345 # Port to listen on
+HOST = '127.0.0.1'
+PORT = 12345
 
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-client_socket.connect((HOST, PORT))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.connect((HOST, PORT))
 
 print(f"Connected to {HOST}:{PORT}")
 
-state = 0
-
-nickname = 'Guest'
-room_name = 'General'
-
 def receive_message():
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        if not message:
-            break
-        print(f"{message}")
-
+        message = server_socket.recv(1024).decode('utf-8')
+        struct = json.loads(message)
+        _type = struct["type"]
+        
+        if _type == "message":
+            print(struct["message"])
+            
 receive_message = threading.Thread(target=receive_message)
 receive_message.daemon = True
 receive_message.start()
 
+nickname = input("Input your nickname: ")
+roomname = input("Input the room you want to enter (Enter for General): ")
+if roomname == '':
+    roomname = 'General'
+    
+struct = {
+    "type" : "connect",
+    "name" : nickname,
+    "room" : roomname
+}
+message = json.dumps(struct).encode('utf-8')
+server_socket.send(message)
+
+
 while True:
-    if state == 0:
-        nickname = input("Input your nickname (or 'exit' to quit):")
-        if nickname.lower() == 'exit':
-            break
-        state = 1
-
-    if state == 1:
-        room_name = input("Input your room (or 'exit' to quit):")
-        if room_name.lower() == 'exit':
-            break
-        state = 2
-        print(f"You are in Room: {room_name}\nFeel free to chat")
-
-        struct = {
-            "type" : "connect",
-            "payload" : {
-                "name" : nickname,
-                "room" : room_name
+    message = input()
+    x = re.search('^upload: .*', message)
+    if x != None:
+        file_path = message[8 : (x.span()[1])]
+        if os.path.isfile(file_path):
+            struct = {
+                "type" : "upload file",
+                "file name" : os.path.basename(file_path),
+                "file size" : os.path.getsize(file_path)
             }
-        }
-
-        message = json.dumps(struct)
-
-        client_socket.send(message.encode('utf-8'))
-
-    if state == 2:
-        message= input("")
-
-        if message.lower() == 'exit':
-            break
-
+            message = json.dumps(struct).encode('utf-8')
+            server_socket.send(message)
+            message = server_socket.recv(1024).decode('utf-8')
+            print("confirmed")
+            struct = json.loads(message)
+            _type = struct["type"]
+            if _type == "upload not permitted":
+                print(f"Failed: {type}\nReason: {struct["reason"]}")
+            elif _type == "upload permitted":
+                print('permission received')
+                f = open(file_path, "rb")
+                file_content = f.read()
+                server_socket.send(file_content)
+                f.close()
+        else:
+            print("The path to this file cannot be found\n")
+    else:
+        x = re.search('^download: .*', message)
+    if x != None:
+        file_name = message[10 : (x.span()[1])]
+        if os.path.isfile(file_path):
+            struct = {
+                "type" : "download file",
+                "file name" : file_name
+            }
+            message= json.dumps(struct).encode('utf-8')
+            server_socket.send(message)
+            message = server_socket.recv(1024).decode('utf-8')
+            struct = json.loads(message)
+            _type = struct["type"]
+            if _type == "file not found":
+                print(f"Failed: {_type}")
+            elif _type == "file found":
+                file_size = struct["file size"]
+                decision = input(f"Download file {file_name} , size: {file_size}. Press (Y/n) to confirm")
+                if decision == '' or decision == 'Y' or decision == 'y':
+                    struct = {
+                        "type" : "download confirm"
+                    }
+                    response_message = json.dumps(struct).encode('utf-8')
+                    server_socket.send(response_message)
+                    file_content = server_socket.recv(file_size)
+                    f = open(file_name, "wb")
+                    f.write(file_content)
+                    f.close()
+                else:
+                    struct = {
+                        "type" : "download abort"
+                    }
+                    response_message = json.dumps(struct).encode('utf-8')
+                    server_socket.send(response_message)
+                    print("File not found on the server media")
+        else:
+            print("The path to this file cannot be found\n")
+    elif message == 'exit':
+        break
+    else:
         struct = {
             "type" : "message",
-            "payload" : {
-                "name" : nickname,
-                "room" : room_name,
-                "message" : message
-            }
+            "message" : message
         }
-
-        message = json.dumps(struct)
-
-        client_socket.send(message.encode('utf-8'))
-
-
-client_socket.close()
+        message = json.dumps(struct).encode('utf-8')
+        server_socket.send(message)
+    
+server_socket.close()
--- lab5/server.py
+++ lab5/server.py
@@ -1,52 +1,132 @@
 import socket
 import threading
 import json
+import os
 
-HOST = '127.0.0.1' # Loopback address for localhost
-PORT = 12345 # Port to listen on
-# Create a socket
+EIGHT_MEGABYTES = 8388608
+
+HOST = '127.0.0.1'
+PORT = 12345
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the specified address and port
+
 server_socket.bind((HOST, PORT))
-# Listen for incoming connections
+
 server_socket.listen()
-print(f"Server is listening on {HOST}:{PORT}")
 
-clients = dict()
-# Function to handle a client's messages
-def handle_client(client_socket, client_address):
+print("Server is listening to 127.0.0.1:12345")
 
-    message = client_socket.recv(1024).decode('utf-8')
-    struct = json.loads(message)
+client_rooms = dict()
 
-    if struct["type"] == "connect":
-        room = struct["payload"]["room"]
-        if not struct["payload"]["room"]:
-            room = "General"
-        if room not in clients.keys():
-            clients[room] = [client_socket]
-        else:
-            clients[room].append(client_socket)
+def send_all(clients : list[socket.socket], buffer, exception=None):
+    print(buffer)
+    for client in clients:
+        if client != exception:
+            client.send(buffer)
 
+def handle_client(client_socket : socket, client_address):
+    name = ''
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-        if not message:
-            break # Exit the loop when the client disconnects 
-        
+        print(f"{client_socket}\n{message}")
         struct = json.loads(message)
-        if struct["type"] == "message":
+        _type = struct["type"]
 
-            message = f"""{struct["payload"]["name"]}: {struct["payload"]["message"]}"""
-                    # Broadcast the message to all clients
-            for client in clients[room]:
-                # if client != client_socket: # find alternative to this IF
-                if client != client_socket:
-                    client.send(message.encode('utf-8'))
-    # Remove the client from the list
-    clients[room].remove(client_socket)
+        if not message:
+            break
+        elif _type == "connect":
+            name = struct["name"]
+            room = struct["room"]
+            if room in client_rooms.keys():
+                client_rooms[room].append(client_socket)
+                struct = {
+                    "type" : "message",
+                    "message" : f"{name} has joined the room"
+                }
+                response_message = json.dumps(struct)
+                send_all(client_rooms[room], response_message.encode('utf-8'), exception=client_socket)
+            else:
+                client_rooms[room] = [client_socket]
+
+        elif _type == "message":
+            message = struct["message"]
+            struct = {
+                "type" : "message",
+                "message" : f"{name}: {message}"
+            }
+            message = json.dumps(struct).encode('utf-8')
+            send_all(client_rooms[room], message, exception=client_socket)
+            
+        elif _type == "upload file":
+            print("uploadddd")
+            file_name = struct["file name"]
+            file_size = struct["file size"]
+            if file_size > EIGHT_MEGABYTES:
+                struct = {
+                    "type" : "upload not permitted",
+                    "reason" : "File too big, can't send files bigger than 8 MB"
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                client_socket.send(response_message)
+            elif file_name in os.listdir("./SERVER_MEDIA"):
+                struct = {
+                    "type" : "upload not permitter",
+                    "reason" : "File with the same name already exists in server media"
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                client_socket.send(response_message)
+            else:
+                struct = {
+                    "type" : "upload permitted"
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                client_socket.send(response_message)
+                print("permission sent")
+                f = open(f"./SERVER_MEDIA/{file_name}", "wb")
+                file_content = client_socket.recv(file_size)
+                print(file_content)
+                f.write(file_content)
+                f.close()
+                struct = {
+                    "type" : "message",
+                    "message" : f"{name} has uploaded {file_name}, {file_size} bytes"
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                send_all(client_rooms[room], response_message)
+            
+        elif _type == "download file":
+            file_name = struct["file name"]
+            files = os.listdir("./SERVER_MEDIA")
+            if file_name in files:
+                file_size = os.path.getsize(file_name)
+                struct = {
+                    "type" : "file found",
+                    "file size" : file_size
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                client_socket.send(response_message)
+                message = client_socket.recv(1024).decode('utf-8')
+                struct = json.loads(message)
+                type = struct["type"]
+                if type == "download confirm":
+                    f = open(f"./SERVER_MEDIA/{file_name}", "rb")
+                    file_content = f.read()
+                    client_socket.send(file_content)
+                    f.close()
+                elif type == "download abort":
+                    pass
+            else:
+                struct = {
+                    "type" : "file not found"
+                }
+                response_message = json.dumps(struct).encode('utf-8')
+                client_socket.send(response_message)
+                pass
+    client_rooms[room].remove(client_socket)
     client_socket.close()
 
 while True:
     client_socket, client_address = server_socket.accept()
+    print(f"{client_socket} connected.")
     client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
     client_thread.start()
